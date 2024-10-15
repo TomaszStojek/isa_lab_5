c8e60614c0bd9cfbc329b9efeb199a04742ecfc4
ankitahlwat <113390178+AnkitAhlwat@users.noreply.github.com>
2024-10-14 23:09:26 -0700

    Fix ssl (#3)

--- server2/server.js
+++ server2/server.js
@@ -10,6 +10,9 @@ const connectionString = process.env.DATABASE_URL;
 
 const client = new Client({
     connectionString: connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 client.connect((err) => {
